6ec83ec45a0ee6ad344c16d1e03dad0b6e958edf
ngocbk <khacngocit@gmail.com>
2024-08-27 21:40:42 +0700

    Update

--- app.js
+++ app.js
@@ -2,16 +2,64 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+const mongoose = require('mongoose');
 
-const BlogPosts = require('./BlogPosts');
+const bodyParser = require('body-parser');
 
-app.get('/', function(red, res){
-    res.send("Server is working!");
-})
-app.get('/posts', function (req, res){
-    res.send(JSON.stringify(BlogPosts.BlogPosts));
-})
+const uri = "mongodb+srv://khacngocptit:khacngocptit@blogserver.zwacg.mongodb.net/?retryWrites=true&w=majority&appName=blogserver"
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.listen(8080, function(){
-    console.log("Server is running on 8080!");
+mongoose.connect(uri,   
+    {
+       useNewUrlParser: true,
+       useUnifiedTopology: true
+   })
+   .then(() => console.log('Connected to MongoDB Atlas'))
+   .catch(err => console.error(err)); 
+   
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
+});
+
+const User = mongoose.model('User', userSchema);   
+    
+
+app.post('/users', async (req, res) => {
+    const { name, email, age } = req.body;
+    
+    const user = new User({ name, email, age });
+    await user.save();
+    res.json(user);
+});
+
+// Get all users
+app.get('/users', async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+});
+
+// Get a user by ID
+app.get('/users/:id', async (req, res) => {
+    const user = await User.findById(req.params.id);   
+    res.json(user);
+});
+
+// Update a user by ID
+app.put('/users/:id', async (req, res) => {
+    const { name, email, age } = req.body;
+    const user = await User.findByIdAndUpdate(req.params.id, { name, email, age }, { new: true });
+    res.json(user);
+});
+
+// Delete a user by ID
+app.delete('/users/:id', async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: 'User deleted' });
+});
+
+app.listen(8080, () => {
+    console.log(`Server listening on port 8080`);
 });
